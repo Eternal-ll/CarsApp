3bdec3ce82a48ab27df38024486dc476ce629ba1
Eternal-ll <aigiz.iskuzhin@yandex.ru>
2022-03-31 18:19:14 +0500

    Easy DB setup

--- CarsApp/Models/Database/CarsAppContext.cs
+++ CarsApp/Models/Database/CarsAppContext.cs
@@ -26,8 +26,10 @@ namespace CarsApp.Models.Database
         {
             if (!optionsBuilder.IsConfigured)
             {
+                var user = "";
+                var pass = "";
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("server=localhost;port=3306;user=гыук;password=password;database=CarsApp", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.28-mysql"));
+                optionsBuilder.UseMySql($"server=localhost;port=3306;user={user};password={pass};database=CarsApp", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.28-mysql"));
             }
         }
 
